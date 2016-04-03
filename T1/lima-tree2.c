ce47070119642292a4dfcbeb66ac3ef42adab5b1
LimaLucas <loliveira@inf.ufsm.br>
2016-04-03 14:56:44 +0000

    Otimização

--- T1/lima-tree2.c
+++ T1/lima-tree2.c
@@ -13,81 +13,25 @@ int main(){
 	struct timespec t0, t1;
 	clock_gettime(CLOCK_MONOTONIC_RAW, &t0);
 
-	int statu;s
+	int status, i;
 	pid_t id;
-
-	printf("\n> P1 = %i\n", getpid());
 
 	id = fork();
 	if(id == -1) return -1;
-	wait(&status);
 
-	if(id == 0){
-		printf("\n > P2 = %i, meu pai é %i\n", getpid(), getppid());
-
-		id = fork();
-		if(id == -1) return -1;
-		wait(&status);
-
+	for(i=1; i<=7; i++){
 		if(id == 0){
-			printf("\n  > P3 = %i, meu pai é %i\n", getpid(), getppid());
-
+			printf("\n > P%i = %i, meu pai é %i\n", i, getpid(), getppid());
 			id = fork();
 			if(id == -1) return -1;
-			wait(&status);
-
-			if(id == 0){
-				printf("\n   > P4 = %i, meu pai é %i\n", getpid(), getppid());
-
-				id = fork();
-				if(id == -1) return -1;
-				wait(&status);
-
-				if(id == 0){
-					printf("\n    > P5 = %i, meu pai é %i\n", getpid(), getppid());
-
-					id = fork();
-					if(id == -1) return -1;
-					wait(&status);
-
-					if(id == 0){
-						printf("\n     > P6 = %i, meu pai é %i\n", getpid(), getppid());
-
-						id = fork();
-						if(id == -1) return -1;
-						wait(&status);
-
-						if(id == 0){
-							printf("\n      > P7 = %i, meu pai é %i\n", getpid(), getppid());
-							printf("\n      > P7 Morreu %i\n", getpid());
-							return 0;
-
-						}else{
-							printf("\n     > P6 Morreu %i\n", getpid());
-							return 0;
-						}
-
-					}else{
-						printf("\n    > P5 Morreu %i\n", getpid());
-						return 0;
-					}
-				}else{
-					printf("\n   > P4 Morreu %i\n", getpid());
-					return 0;
-				}
-			}else{
-				printf("\n  > P3 Morreu %i\n", getpid());
-				return 0;
-			}
 		}else{
-			printf("\n > P2 Morreu %i\n", getpid());
+			wait(&status);
+			printf("\n > P%i Morreu %i\n", i, getpid());
+			if(i == 1){
+				clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
+			 	printf("\n Tempo de execução: %lf\n\n", difTime(t0, t1));
+			}
 			return 0;
 		}
-
-	}else{
-		printf("\n> P1 Morreu %i\n", getpid());
-		clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
-		printf("\nTempo de execução: %lf\n\n", difTime(t0, t1));
-		return 0;
 	}
 }
